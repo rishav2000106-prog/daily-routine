c8025f9db65e40d5696628a94d6944ccd2ef63c2
rishav2000106-prog <rishav2000106-prog@users.noreply.github.com>
2026-04-29 23:15:39 +0530

    Added PWA support (Manifest, Service Worker, Icon)

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'routineos-v1';
+const ASSETS = [
+  '/daily-routine/',
+  '/daily-routine/index.html',
+  '/daily-routine/index.css',
+  '/daily-routine/app.js',
+  '/daily-routine/icon.svg',
+  '/daily-routine/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
